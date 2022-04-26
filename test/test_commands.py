bc5ea786d8aabce30f5166103972e7222bd79a80
aleksandra <gapon-sasha@mail.ru>
2022-04-27 00:16:45 +0300

    add test for error commands

--- test/test_commands.py
+++ test/test_commands.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from src.commands import cat, echo, exit, pwd, wc
 from src.cmd_work.shell import Shell_state
+from src.errors.errors import CommandArgumentsError
 
 import os.path
 
@@ -13,6 +14,11 @@ class TestCommands(TestCase):
         cat_res = cat_com.run([], self.exit_state)
         expected_res = ["haha", "hahahaha", "it is test file only", "a a a a"]
         self.assertEqual(cat_res, expected_res)
+
+    def test_cat_no_file(self):
+        cat_com = cat.Cat_inner("hell_file.txt")
+        with self.assertRaises(FileNotFoundError):
+            cat_res = cat_com.run([], self.exit_state)
 
     def test_echo(self):
         echo_com = echo.Echo_inner("a", "b", "c", "abc")
@@ -32,7 +38,23 @@ class TestCommands(TestCase):
         expected_res = ['4\t11\t42\thelp_file.txt']
         self.assertEqual(wc_res, expected_res)
 
+    def test_wc_no_file(self):
+        wc_com = wc.Wc_inner("hell_file.txt")
+        with self.assertRaises(FileNotFoundError):
+            wc_com.run([], self.exit_state)
+
     def test_exit(self):
         exit_cmd = exit.Exit_inner()
-        exit_res = exit_cmd.run([], self.exit_state)
-        self.assertEqual(True, self.exit_state.get_state())
+        exit_cmd.run([], self.exit_state)
+        self.assertTrue(self.exit_state.get_state())
+
+    def test_wrong_arguments(self):
+        cat_com = cat.Cat_inner()
+        with self.assertRaises(CommandArgumentsError):
+            cat_com.run([], self.exit_state)
+
+        echo_com = echo.Echo_inner()
+        with self.assertRaises(CommandArgumentsError):
+            echo_com.run([], self.exit_state)
+
+
